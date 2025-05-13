4477849997c5c0d910f1d7f58237bc9461abd7a6
Anesu Gandiwa <148047827+Anesugandiwa@users.noreply.github.com>
2025-05-13 19:27:39 +0200

    Payments
    Payments done

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -82,4 +82,304 @@ class PagesController extends Controller
 
     }
 
+
+
+    public function recover(){
+        return inertia('recover');
+    }
+
+    public function recoverTicket($ref_no){
+        $paymen = Payment::where('referenceNumber', $ref_no)->with(['ticket'])->get();
+        if($paymen->is_paid ==1){
+            return inertia('Recovered', [
+                'tickets' => Purchase::where('referenceNumber',$ref_no)->with(['ticket'])->get(),
+                'referenceNumber' => $ref_no,
+            ]);
+
+
+        } else {
+            return inertia('Hanging', [
+                'reference' => $ref_no,
+            ]);
+        }
+    }
+
+    public function initiatePayment(Request $request){
+        if(env('App_DEBUG')){
+            $referenceNumber = rand(1, 50);
+
+            $paymentSaved = new Payment();
+
+            $paymentSaved->referenceNumber  = $referenceNumber;
+            $paymentSaved->pollUrl          = '/fake-urd';
+            $paymentSaved->user_id          = auth()->user()->id;
+            $paymentSaved->save();
+
+            return response()->json([
+                'ref_num' => $referenceNumber,
+                'pollUrl' => '/fake-urd',
+                'status'  => 'success',
+                'message' => 'Please Enter pin on your phone',
+            ]);
+
+
+
+        }
+
+        $pesepay = new Pesepay(
+            "34bebfa7-ac62-4f7d-a4fc-5231b288d3a4",
+            "69df0e53d55748a79bc5cc9ba9e614db"
+        );
+
+        $pesepay->returnUrl =  route('pese-return');
+        $pesepay->resultUrl =  route('pese-result',now());
+
+        $requiredFields = ['customerPhoneNumber'=>$request->customerPhoneNumber];
+
+        $payment = $pesepay->createPayment(
+            'USD',
+            'PZW211',
+            auth()->user()->email,
+            $request->customerPhoneNumber,
+            auth()->user()->name
+        );
+
+        $response = $pesepay->makeSeamlessPayment(
+            $payment,
+            $request->reason,
+            \Cart::session(auth()->user()->id)->getTotal(),
+            $requiredFields,
+            auth()->user()->id.'-'.now()
+        );
+
+        
+        if ($response->success()) {
+            # Save the reference number and/or poll url (used to check the status of a transaction)
+            $referenceNumber = $response->referenceNumber();
+            $pollUrl = $response->pollUrl();
+
+            $paymentSaved = new Payment();
+
+            $paymentSaved->referenceNumber  = $referenceNumber;
+            $paymentSaved->pollUrl          = $pollUrl;
+            $paymentSaved->user_id          = auth()->user()->id;
+            $paymentSaved->save();
+
+            return response()->json([
+                'ref_num'      => $referenceNumber,
+                'pollUrl'      => $pollUrl,
+                'status'       => 'success',
+                'message'      => 'Please enter pin on your phone'
+            ]);
+
+        } else {
+            #Get Error Message
+            $errorMessage = $response->message();
+
+           return response()->json([
+                'status'       => 'failed',
+                'message'      => $errorMessage
+            ]);
+        }
+
+    }
+
+    public function pesePayReturn(Request $request){
+        
+        if(env('APP_DEBUG')){
+            
+            $paymen = Payment::where('referenceNumber',$request->ref_num)->first();
+            $paymen->is_paid = 1;
+            $paymen->save();
+            
+            return response()->json([
+                'status'       => 'paid',
+                'ref_num'      => $request->ref_num,
+                'message'      => 'Payment Complete'
+             ]);
+            }
+
+            $pesepay = new Pespay(
+                "34bebfa7-ac62-4f7d-a4fc-5231b288d3a4",
+                "69df0e53d55748a79bc5cc9ba9e614db"
+            );
+
+            $response = $pesepay->checkPayment($request->ref_num);
+
+            if ($response->success()){
+                if($response->paid()){
+                    $paymen = Payment::where('referenceNumber', $request->ref_num)->first();
+                    $paymen->is_paid = 1;
+                    $paymen->save();
+
+                     return response()->json([
+                         'status'       => 'paid',
+                         'ref_num'      => $request->ref_num,
+                         'message'      => 'Payment Complete'
+                     ]);
+                } else{
+                    return response()->json([
+                        'status' => 'pending',
+                        'message' => 'pending please wait'
+                    ]);
+                }
+            }  else {
+                 # Get error message
+                 return response()->json([
+                     'status'      => 'failed',
+                     'message'     =>  $response->message()
+                 ]);
+             }
+
+
+    }
+    public function pesePayResult($id){
+        $paymen = Payment::where('referenceNumber',$id)->first();
+
+        if($paymen->is_paid ==1){
+            if(count(Purchase::where('referenceNumber', $id)->get())===0){
+                $items = \Cart::session(auth()->user()->id)->getContent();
+                $tickets = [];
+                
+                foreach($items as $item){
+                    for($i =0; i< $item->quantity; $i++){
+                        $objct = new \stdClass();
+
+                        $objct->id          =$item->id.'_'.$i;
+                        $objct->name        =$item->name;     
+                        $objct->image        =$item->associatedModel->image;
+                        $objct->event_id   =$item->associatedModel->event_id;
+                        $objct->price     = $item->price;
+                        $objct->event     = Event::find($item->associatedModel->event_id);
+
+                        array_push( $tickets, $objct);
+
+                        $name = rand(1001,20458).$objct->id;
+
+                        QRCode::text($id)
+                             ->setOutfile(Storage::disk("public")->path("tickets/purchases/".$name.".svg"))
+                             ->svg();
+
+                        $purchase = new Purchase();
+
+                        $purchase->event_id  = $objct->event_id;
+                        $purchase->ticket_id          = $item->id;
+                        $purchase->user_id            = auth()->user()->id;
+                        $purchase->quantity           = 1;
+                        $purchase->referenceNumber    = $id;
+                        $purchase->qr_path            = "/storage/tickets/purchases/".$name.".svg";
+                        $purchase->status             = 0;
+                        $purchase->save();
+                        \Cart::session(auth()->user()->id)->remove($item->id);
+                    }
+                }
+            }
+
+            Auth::logout();
+            return inertia(' PaymentSuccess',[
+                'tickets' => Purchase::where('referenceNumber', $id)
+                    ->with(['ticket'])
+                    ->get(),
+                'referenceNumber' => $id,
+            ]);
+ 
+        }else{
+            return inertia('Hanging', [
+                'rerference' =>$id,
+            ]);
+        }
+    }
+        public function verifyTicket($id)
+    {
+        return Purchase::where('referenceNumber',$id)->where('status',0)->first();
+    }
+
+    public function pesePayCheckout(){
+        $pesepay = new Pesepay(
+            "34bebfa7-ac62-4f7d-a4fc-5231b288d3a4",
+            "69df0e53d55748a79bc5cc9ba9e614db"
+        );
+
+        $pesepay->returnUrl =  route('pesePayReturnExternal');
+        $pesepay->resultUrl =  route('pese-result',now());
+
+        $transaction = $pesepay->createTransaction(
+            \Cart::session(auth()->user()->id)->getTotal(),
+            'USD',
+            'Purchase of a ticket',
+            'MERCHANT_REFERENCE_'.now()
+        );
+
+        $response = $pesepay->initiateTransaction($transaction);
+
+        if ($response->success()) {
+            # Save the reference number and/or poll url (used to check the status of a transaction)
+            $referenceNumber = $response->referenceNumber();
+            $pollUrl = $response->pollUrl();
+
+
+            $paymentSaved = new Payment();
+
+            $paymentSaved->referenceNumber  = $referenceNumber;
+            $paymentSaved->pollUrl          = $pollUrl;
+            $paymentSaved->user_id          = auth()->user()->id;
+            $paymentSaved->save();
+
+            # Get the redirect url and redirect user to complete transaction
+            $redirectUrl = $response->redirectUrl();
+
+            //add reference to session
+            session(['referenceNumber' => $referenceNumber]);
+            return inertia_location($redirectUrl);
+
+        } else {
+            # Get error message
+            $errorMessage = $response->message();
+
+            return $errorMessage;
+        }
+    }
+     public function  pesePayReturnExternal(){
+
+        if(env('APP_DEBUG')){
+
+            $paymen = Payment::where('referenceNumber',session('referenceNumber'))->first();
+            $paymen->is_paid = 1;
+            $paymen->save();
+            return  redirect()->route('pese-result',session('referenceNumber'));
+        }
+
+        $pesepay = new Pesepay(
+            "34bebfa7-ac62-4f7d-a4fc-5231b288d3a4",
+            "69df0e53d55748a79bc5cc9ba9e614db"
+        );
+
+        $response = $pesepay->checkPayment(session('referenceNumber'));
+
+        if ($response->success()) {
+
+            if ($response->paid()) {
+
+                $paymen = Payment::where('referenceNumber',session('referenceNumber'))->first();
+                $paymen->is_paid = 1;
+                $paymen->save();
+
+                return  redirect()->route('pese-result',session('referenceNumber'));
+
+            }else{
+
+                return Inertia::render('PaymentFailed');
+            }
+
+        } else {
+            # Get error message
+            return response()->json([
+                'status'      => 'failed',
+                'message'     =>  $response->message()
+            ]);
+        }
+
+    }
+
+
 }
---
+++ resources/js/pages/PaymentFailed.vue
@@ -0,0 +1,32 @@
+<script setup></script>
+<template>
+    <v-container>
+        <div class="glass pa-3">
+            <v-card variant="flat" class="text-center max-auto" max-width="500" color="error">
+
+                <svg xmlns="http://www.w3.org/2000/svg"
+                    width="50px"
+                    height="50px"
+                    viewBox="0 0 24 24"
+               ><g fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="M12 8v4"/><circle cx="12" cy="15" r="1" fill="currentColor"/><path stroke="currentColor" stroke-linecap="round" stroke-width="1.5" d="M3 10.417c0-3.198 0-4.797.378-5.335c.377-.537 1.88-1.052 4.887-2.081l.573-.196C10.405 2.268 11.188 2 12 2s1.595.268 3.162.805l.573.196c3.007 1.029 4.51 1.544 4.887 2.081C21 5.62 21 7.22 21 10.417v1.574c0 2.505-.837 4.437-2 5.913M3.193 14c.857 4.298 4.383 6.513 6.706 7.527c.721.315 1.082.473 2.101.473c1.02 0 1.38-.158 2.101-.473c.579-.252 1.231-.58 1.899-.994"/></g></svg>
+
+               <v-card-title class="text-center">
+                Payment Failed
+               </v-card-title>
+               <v-card-text>
+                Please try again
+
+               </v-card-text>
+               <v-card-actions>
+                   <InertiaLink :href="route('cart')" class="InertiaLink">
+                       <v-btn block variant="outlined" size="large" color="white"  >
+                           Go to Cart
+                       </v-btn>
+                   </InertiaLink>
+                </v-card-actions>
+
+            </v-card>
+
+        </div>
+    </v-container>
+</template>
